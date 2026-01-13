6d868d76b45b138f4cc5f53d013b18eebe1aa16c
Boya Mahesh <63939597+Mahiboya427@users.noreply.github.com>
2026-01-13 15:38:25 +0530

    Refactor customActivity.js for improved structure

--- public/customActivity.js
+++ public/customActivity.js
@@ -1,18 +1,12 @@
-window.onload = function () {
-  const connection = new Postmonger.Session();
-  let payload = {};
+require(["postmonger"], function(Postmonger) {
+  var connection = new Postmonger.Session();
+  connection.trigger("ready");
 
-  connection.trigger('ready');
-
-  connection.on('initActivity', function (data) {
-    payload = data || {};
+  connection.on("initActivity", function(data){
+    var payload = data || {};
+    payload.metaData = payload.metaData || {};
+    payload.metaData.isConfigured = true;
+    payload.name = "Daytime Window Check";
+    connection.trigger("updateActivity", payload);
   });
-
-  connection.on('clickedNext', function () {
-
-    payload.metaData = { isConfigured: true };
-    payload.name = 'Daytime Window Check';
-
-    connection.trigger('updateActivity', payload);
-  });
-};
+});
