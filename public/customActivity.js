7089f04a4de59a3955b8a3f9e63612a951c7f1c0
Boya Mahesh <63939597+Mahiboya427@users.noreply.github.com>
2026-01-13 15:23:57 +0530

    Update customActivity.js

--- public/customActivity.js
+++ public/customActivity.js
@@ -4,27 +4,14 @@ window.onload = function () {
 
   connection.trigger('ready');
 
-  connection.on('initActivity', data => {
+  connection.on('initActivity', function (data) {
     payload = data || {};
-
-    const savedCountry =
-      payload?.arguments?.execute?.inArguments?.find(a => a.country)?.country;
-
-    if (savedCountry) document.getElementById('country').value = savedCountry;
   });
 
-  connection.on('clickedNext', () => {
-    const country = document.getElementById('country').value;
+  connection.on('clickedNext', function () {
 
-    payload.arguments = payload.arguments || {};
-    payload.arguments.execute = payload.arguments.execute || {};
-
-    const args = payload.arguments.execute.inArguments || [];
-    payload.arguments.execute.inArguments =
-      args.filter(a => !a.country).concat([{ country, namespace: 'Activity' }]);
-
-    payload.metaData = { isConfigured: true, label: `Country: ${country}` };
-    payload.name = `Window Check (${country})`;
+    payload.metaData = { isConfigured: true };
+    payload.name = 'Daytime Window Check';
 
     connection.trigger('updateActivity', payload);
   });
